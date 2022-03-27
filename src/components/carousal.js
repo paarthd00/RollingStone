03825c10389206ba9c3048c8b351453594ceac12
paarthd00 <paarthcode@gmail.com>
2022-03-26 18:08:27 -0700

    fonts

--- src/components/carousal.js
+++ src/components/carousal.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import FirstPage from './firstpage'
 // import SecondPage from './middlepage'
-// import EndPage from './lastpage'
+import EndPage from './lastpage'
 import Nwa from '../images/nwa.jpg'
 import songs from '../data/songs'
 import Sound from 'react-sound'
@@ -54,7 +54,7 @@ const CarousalPage = () =>{
         <Carousel fade interval={null}>
         <Carousel.Item>
             <div className="page">
-                <p className="billboard" style={{fontFamily:"'Bangers', cursive"}}>#1 Billboard!</p>
+                <p className="billboard" style={{fontFamily: "'Space Mono', monospace"}}>#1 Billboard!</p>
                 <div className="songnames" style={{cursor: "pointer",position:"absolute", zIndex:10000, color: "yellow"}}>  {songName}</div>
                 <p className="rollingstone" style={{fontFamily:"'Nosifer', cursive"}}>The Rolling Stone</p>
                 <img src={Nwa} style={{position: "absolute", width: "55vw", opacity:.7}} alt="album-cover"></img>
@@ -77,16 +77,14 @@ const CarousalPage = () =>{
             <FirstPage />   
             
         </Carousel.Item>
-        {/* <Carousel.Item>
-            <SecondPage />
-        </Carousel.Item>
+
         <Carousel.Item>
             <EndPage />
             <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
             </Carousel.Caption>
-        </Carousel.Item> */}
+        </Carousel.Item>
         </Carousel>
     )
 }
